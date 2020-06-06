b7b098abf1f5798f4f741e98e1377c21065db91d
Jaime <jolivatorres25@gmail.com>
2020-06-06 18:59:41 -0500

    Se permite simular un arreglo de divisas de forma secuencial

--- source/main.py
+++ source/main.py
@@ -8,11 +8,19 @@ from strategies.strategy import *
 def main():
     #parser = argparse.ArgumentParser(description='Backtesting process to evaluate the efficency of forex strategies.')
     #Simulation.run();
-    AUDUSD = BacktestingDataframe("AUDUSD").get_dataframe()
-    print(AUDUSD)
-    bt = Backtest(AUDUSD,SmaCross,cash=10000, commission=.002)
-    bt.run()
-    bt.plot()
+    #AUDUSD = BacktestingDataframe("AUDUSD","27-05-2020").get_dataframe()
+    currencies = ["XAUUSD","AUDUSD","NZDUSD","EURUSD","USDJPY","EURJPY","GBPUSD"]
+    run_array_simulation(SmaCross,10000,.002,currencies)
+
+def run_array_simulation(strategy,cash,commission,currencies,dates = None):
+    if dates is None:
+        for currency in currencies:
+            df = BacktestingDataframe(currency).get_dataframe()
+            bt = Backtest(df,strategy,cash=cash, commission=commission)
+            print(bt.run())
+            bt.plot(filename="{currency}.html".format(currency = currency),open_browser=False)
+    else:
+        pass
 
 if __name__ == '__main__':
     main()
