008aef98512e13d23f972714f42e5fcabe764473
Jaime <jolivatorres25@gmail.com>
2020-06-08 14:03:35 -0500

    Se migra la logica de la simulacion hacia un archivo propio para mejorar la mantenibilidad

---
+++ source/core/simulation.py
@@ -0,0 +1,17 @@
+from core.csvreader import BacktestingDataframe
+from backtesting import Backtest, Strategy
+
+def run_array_simulation(strategy,cash,commission,currencies,dates = None):
+    if dates is None:
+        for currency in currencies:
+            df = BacktestingDataframe(currency).get_dataframe()
+            bt = Backtest(df,strategy,cash=cash, commission=commission)
+            print(bt.run())
+            bt.plot(filename="{currency}.html".format(currency = currency),open_browser=False)
+    else:
+        for date in dates:
+            for currency in currencies:
+                df = BacktestingDataframe(currency,paramdate = date).get_dataframe()
+                bt = Backtest(df,strategy,cash=cash, commission=commission)
+                print(bt.run())
+                bt.plot(filename="{currency}.html".format(currency = currency),open_browser=False)
