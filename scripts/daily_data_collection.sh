3bb7a51d4a4b8ce3723f52d3c4ed922488e3de04
Jaime <jolivatorres25@gmail.com>
2020-06-17 20:54:38 -0500

    Se modifica la ruta de data para mejorar interoperabilidad

--- scripts/daily_data_collection.sh
+++ scripts/daily_data_collection.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-duka EURUSD -c M1 --f $HOME/trading/trading-para-todos/data --header
-echo -ne '\n' | duka XAUUSD -c M1 --f $HOME/trading/trading-para-todos/data --header
-echo -ne '\n' | duka AUDUSD -c M1 --f $HOME/trading/trading-para-todos/data --header
-echo -ne '\n' | duka NZDUSD -c M1 --f $HOME/trading/trading-para-todos/data --header
-echo -ne '\n' | duka USDJPY -c M1 --f $HOME/trading/trading-para-todos/data --header
-echo -ne '\n' | duka EURJPY -c M1 --f $HOME/trading/trading-para-todos/data --header
-echo -ne '\n' | duka GBPUSD -c M1 --f $HOME/trading/trading-para-todos/data --header
+duka EURUSD -c M1 --f $HOME/trading-para-todos/data --header
+echo -ne '\n' | duka XAUUSD -c M1 --f $HOME/trading-para-todos/data --header
+echo -ne '\n' | duka AUDUSD -c M1 --f $HOME/trading-para-todos/data --header
+echo -ne '\n' | duka NZDUSD -c M1 --f $HOME/trading-para-todos/data --header
+echo -ne '\n' | duka USDJPY -c M1 --f $HOME/trading-para-todos/data --header
+echo -ne '\n' | duka EURJPY -c M1 --f $HOME/trading-para-todos/data --header
+echo -ne '\n' | duka GBPUSD -c M1 --f $HOME/trading-para-todos/data --header
