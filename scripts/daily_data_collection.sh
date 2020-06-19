ff65704bb423e8c75817a0a545f475fe9a1638a4
Jaime <jolivatorres25@gmail.com>
2020-06-19 18:31:27 -0500

    Se actualiza el script para mejorar interoperabilidad

--- scripts/daily_data_collection.sh
+++ scripts/daily_data_collection.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-duka EURUSD -c M1 --f $HOME/trading-para-todos/data --header
-echo -ne '\n' | duka XAUUSD -c M1 --f $HOME/trading-para-todos/data --header
-echo -ne '\n' | duka AUDUSD -c M1 --f $HOME/trading-para-todos/data --header
-echo -ne '\n' | duka NZDUSD -c M1 --f $HOME/trading-para-todos/data --header
-echo -ne '\n' | duka USDJPY -c M1 --f $HOME/trading-para-todos/data --header
-echo -ne '\n' | duka EURJPY -c M1 --f $HOME/trading-para-todos/data --header
-echo -ne '\n' | duka GBPUSD -c M1 --f $HOME/trading-para-todos/data --header
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+duka EURUSD -c M1 --f $SCRIPTPATH/../data --header
+echo -ne '\n' | duka XAUUSD -c M1 --f $SCRIPTPATH/../data --header
+echo -ne '\n' | duka AUDUSD -c M1 --f $SCRIPTPATH/../data --header
+echo -ne '\n' | duka NZDUSD -c M1 --f $SCRIPTPATH/../data --header
+echo -ne '\n' | duka USDJPY -c M1 --f $SCRIPTPATH/../data --header
+echo -ne '\n' | duka EURJPY -c M1 --f $SCRIPTPATH/../data --header
+echo -ne '\n' | duka GBPUSD -c M1 --f $SCRIPTPATH/../data --header
